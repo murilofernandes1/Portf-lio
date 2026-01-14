50062b781df62602a5e57a343c89380b66162667
Murilo <murigod8@gmail.com>
2026-01-14 20:27:30 -0300

    feat: finished main layout

--- src/components/left-navbar/navbar.tsx
+++ src/components/left-navbar/navbar.tsx
@@ -1,29 +1,62 @@
+"use client";
+
 import styles from "./navbar.module.css";
 import Link from "next/link";
-export default function NavBar() {
-  return (
-    <>
-      <div className={styles.container}>
-        <div className={styles.navbar}>
-          <Link href={"/projects"} className={styles.page}>
-            Projetos
-          </Link>
-          <Link href={"/technologies"} className={styles.page}>
-            Tecnologias e Ferramentas
-          </Link>
+import { usePathname } from "next/navigation";
 
-          <Link href={"/education"} className={styles.page}>
-            Educação
-          </Link>
-          <Link href={"/contact"} className={styles.page}>
-            Contato
-          </Link>
-          <Link href={"/about"} className={styles.page}>
-            Sobre mim
-          </Link>
-        </div>
-        <div className={styles.divisor} />
+export default function NavBar() {
+  const pathname = usePathname();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.navbar}>
+        <Link
+          href="/projects"
+          className={`${styles.page} ${
+            pathname === "/projects" ? styles.active : ""
+          }`}
+        >
+          Projetos
+        </Link>
+
+        <Link
+          href="/technologies"
+          className={`${styles.page} ${
+            pathname === "/technologies" ? styles.active : ""
+          }`}
+        >
+          Tecnologias e Ferramentas
+        </Link>
+
+        <Link
+          href="/education"
+          className={`${styles.page} ${
+            pathname === "/education" ? styles.active : ""
+          }`}
+        >
+          Educação
+        </Link>
+
+        <Link
+          href="/contact"
+          className={`${styles.page} ${
+            pathname === "/contact" ? styles.active : ""
+          }`}
+        >
+          Contato
+        </Link>
+
+        <Link
+          href="/about"
+          className={`${styles.page} ${
+            pathname === "/about" ? styles.active : ""
+          }`}
+        >
+          Sobre mim
+        </Link>
       </div>
-    </>
+
+      <div className={styles.divisor} />
+    </div>
   );
 }
