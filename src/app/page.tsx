2794176801c5f3110a241683e7b474db3957b6b1
Murilo <murigod8@gmail.com>
2026-01-14 22:24:57 -0300

    styles: finished home/landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,16 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <div className={styles.intro}></div>
-      </main>
-    </div>
+    <main className={styles.main}>
+      <h1 className={styles.bigname}>Murilo Fernandes Vaz</h1>
+      <h2 className={styles.role}>Desenvolvedor Full Stack</h2>
+      <h3 className={styles.message}>
+        Transformo ideias em aplicações sólidas e modernas
+        <br />
+        <span className={styles.highlight}>Do Mobile ao Desktop.</span>
+        <br />
+        Arquitetura, performance e detalhe em cada linha.
+      </h3>
+    </main>
   );
 }
