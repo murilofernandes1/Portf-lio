e5193ea1843f01f39345321d11a071f1d3411c1d
Murilo <murigod8@gmail.com>
2026-01-16 18:36:00 -0300

    feat: finished mobile nav

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,29 +1,40 @@
+"use client";
+
+import { useState } from "react";
 import Link from "next/link";
 import styles from "./header.module.css";
-import { Github, Linkedin, Mail } from "lucide-react";
+import { Github, Linkedin, Mail, Menu } from "lucide-react";
+import MobileNavbar from "../mobile-navbar/mobile-navbar";
+
 export default function Header() {
+  const [navOpen, setNavOpen] = useState(false);
+
   return (
     <>
       <div className={styles.container}>
-        <Link href={"/"} scroll className={styles.name}>
-          MFV
-        </Link>
+        <div className={styles["initial-icons"]}>
+          <button className={styles.nav} onClick={() => setNavOpen(!navOpen)}>
+            <Menu className={styles.icon} />
+          </button>
+          <Link href="/" scroll className={styles.name}>
+            MFV
+          </Link>
+        </div>
+
+        {navOpen && <MobileNavbar navOpen={navOpen} setNavOpen={setNavOpen} />}
 
         <div className={styles.navbar}>
-          <Link href={"https://github.com/murilofernandes1"}>
-            <Github className={styles.icon} size={30} />
+          <Link href="https://github.com/murilofernandes1">
+            <Github className={styles.icon} />
           </Link>
-
-          <Link href={"https://www.linkedin.com/in/murilofernandesdev/"}>
-            <Linkedin className={styles.icon} size={30} />
+          <Link href="https://www.linkedin.com/in/murilofernandesdev/">
+            <Linkedin className={styles.icon} />
           </Link>
-
-          <Link href={"mailto:murilofernandesvaz4@gmail.com"}>
-            <Mail className={styles.icon} size={30} />
+          <Link href="mailto:murilofernandesvaz4@gmail.com">
+            <Mail className={styles.icon} />
           </Link>
         </div>
       </div>
-      <div className={styles.divisor} />
     </>
   );
 }
---
+++ src/components/mobile-navbar/mobile-navbar.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import styles from "./mobile-navbar.module.css";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+type MobileNavbarProps = {
+  navOpen: boolean;
+  setNavOpen: (open: boolean) => void;
+};
+
+export default function MobileNavbar({
+  navOpen,
+  setNavOpen,
+}: MobileNavbarProps) {
+  const pathname = usePathname();
+
+  return (
+    <>
+      <div
+        className={`${styles.overlay} ${navOpen ? styles.show : ""}`}
+        onClick={() => setNavOpen(false)}
+      />
+      <nav className={`${styles.container} ${navOpen ? styles.open : ""}`}>
+        <div className={styles.navbar}>
+          <Link
+            href="/projects"
+            className={`${styles.page} ${pathname === "/projects" ? styles.active : ""}`}
+            onClick={() => setNavOpen(false)}
+          >
+            Projetos
+          </Link>
+          <Link
+            href="/technologies"
+            className={`${styles.page} ${pathname === "/technologies" ? styles.active : ""}`}
+            onClick={() => setNavOpen(false)}
+          >
+            Tecnologias
+          </Link>
+          <Link
+            href="/contact"
+            className={`${styles.page} ${pathname === "/contact" ? styles.active : ""}`}
+            onClick={() => setNavOpen(false)}
+          >
+            Contato
+          </Link>
+          <Link
+            href="/about"
+            className={`${styles.page} ${pathname === "/about" ? styles.active : ""}`}
+            onClick={() => setNavOpen(false)}
+          >
+            Sobre mim
+          </Link>
+        </div>
+      </nav>
+    </>
+  );
+}
